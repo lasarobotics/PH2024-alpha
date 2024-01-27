801e558026313f93cd87289d4096755563de4658
Arun Varadarajan <arunieboy@gmail.com>
2024-01-27 15:23:51 -0600

    modfied shootersubsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -18,6 +19,13 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private static final DriveSubsystem DRIVE_SUBSYSTEM = new DriveSubsystem(DriveSubsystem.initializeHardware());
+  private static final ShooterSubsystem SHOOTER_SUBSYSTEM = new ShooterSubsystem(
+    ShooterSubsystem.initHardware(),
+    Constants.Shooter.SHOOTER_PID,
+    Constants.Shooter.SHOOTER_SPEED,
+    Constants.Shooter.INTAKE_SPEED,
+    Constants.Shooter.SPIT_SPEED
+  );
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController PRIMARY_CONTROLLER =
--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -7,62 +7,98 @@ import java.util.function.DoubleSupplier;
 
 import org.lasarobotics.hardware.revrobotics.Spark;
 import org.lasarobotics.hardware.revrobotics.Spark.MotorKind;
+import org.lasarobotics.utils.PIDConstants;
 
 import com.revrobotics.CANSparkBase.ControlType;
-import com.revrobotics.SparkPIDController.ArbFFUnits;
 
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Unit;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.Velocity;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase implements AutoCloseable {
 
   public static class Hardware {
-    private Spark masterMotor, slaveMotor;
 
-    public Hardware(Spark masterMotor, Spark slaveMotor) {
-      this.masterMotor = masterMotor;
-      this.slaveMotor = slaveMotor;
+    private Spark shooterMotor, indexerMotor;
+
+    public Hardware(Spark shooterMotor, Spark indexerMotor) {
+      this.shooterMotor = shooterMotor;
+      this.indexerMotor = indexerMotor;
     }
   }
 
-  private Spark m_masterMotor, m_slaveMotor;
+  private Spark m_shooterMotor, m_indexerMotor;
+  private Measure<Velocity<Angle>> m_shooterSpeed;
+  private double m_intakeSpeed;
+  private double m_spitSpeed;
 
-  public ShooterSubsystem(Hardware shooterHardware) {
-    m_masterMotor = shooterHardware.masterMotor;
-    m_slaveMotor = shooterHardware.slaveMotor;
 
-    m_slaveMotor.follow(m_masterMotor);
+  public ShooterSubsystem(Hardware shooterHardware, PIDConstants pidf, Measure<Velocity<Angle>> shooterSpeed, double intakeSpeed, double spitSpeed) {
+    m_shooterMotor = shooterHardware.shooterMotor;
+    m_indexerMotor = shooterHardware.indexerMotor;
+    m_shooterSpeed = shooterSpeed;
+    m_intakeSpeed = intakeSpeed;
+    m_spitSpeed = spitSpeed;
+
   }
 
   public static Hardware initHardware() {
     return new Hardware(
       new Spark(Constants.ShootHardware.MASTER_MOTOR_ID, MotorKind.NEO), 
-      new Spark(Constants.ShootHardware.SLAVE_MOTOR_ID, MotorKind.NEO)
+      new Spark(Constants.ShootHardware.INDEXER_MOTOR_ID, MotorKind.NEO)
     );
   }
 
-  public void shoot(double power) {
-    m_masterMotor.set(power, ControlType.kDutyCycle);
-  }
-  public void intake(double power) {
-    m_masterMotor.set(-power, ControlType.kDutyCycle);
+  private void shoot() {
+    m_shooterMotor.set(m_shooterSpeed.in(Units.RPM), ControlType.kVelocity);
   }
 
-  public void stop(){
-    m_masterMotor.stopMotor();
+  private void spit() { 
+    m_shooterMotor.set(+m_spitSpeed, ControlType.kDutyCycle);
+    m_indexerMotor.set(+m_spitSpeed, ControlType.kDutyCycle);
   }
+
+  private void feed() {
+    m_indexerMotor.set(
+      m_intakeSpeed, ControlType.kDutyCycle);
+  }
+
+  private void intake() {
+    m_shooterMotor.set(-m_intakeSpeed, ControlType.kDutyCycle);
+    m_indexerMotor.set(-m_intakeSpeed, ControlType.kDutyCycle);
+  }
+  private void stop() {
+    m_shooterMotor.stopMotor();
+    m_indexerMotor.stopMotor();
+  }
+
+  public boolean isFlyWheelAtSpeed() {
+    return Math.abs(m_shooterMotor.getInputs().encoderVelocity - Constants.Shooter.DESIRED_RPM) <= 50;
+  }
+
+
   @Override
   public void close() {
-     m_masterMotor.close();
-     m_slaveMotor.close();
+     m_shooterMotor.close();
+     m_indexerMotor.close();
   }
 
-  public Command shootCommand(DoubleSupplier speed) {
-    return startEnd(() -> shoot(speed.getAsDouble()), () -> stop());
+  public Command shootCommand() {
+    return Commands.parallel(
+      run(() -> shoot()),
+      run(() -> {
+        if (isFlyWheelAtSpeed()) feed();
+      })
+    );
   }
-  public Command intakeCommand(DoubleSupplier speed) {
-    return startEnd(() -> intake(speed.getAsDouble()), () -> stop());
+  public Command intakeCommand() {
+    return startEnd(() -> intake(), () -> stop());
   }
 
   public Command stopCommand() {
@@ -72,7 +108,8 @@ public class ShooterSubsystem extends SubsystemBase implements AutoCloseable {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    m_shooterMotor.periodic();
+    m_indexerMotor.periodic();
   }
-
 
 }
