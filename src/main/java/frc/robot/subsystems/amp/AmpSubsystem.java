64af21cc791fdfad303cf7b3741fe2e53c9aa7b3
rachitkakkar <rachitkakkar7@gmail.com>
2024-01-28 18:19:04 -0600

    Quality control

--- src/main/java/frc/robot/subsystems/amp/AmpSubsystem.java
+++ src/main/java/frc/robot/subsystems/amp/AmpSubsystem.java
@@ -13,78 +13,66 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class AmpSubsystem extends SubsystemBase {
-    public static class Hardware {
-        private Spark ampMotor;
+  public static class Hardware {
+    private Spark ampMotor;
 
-        public Hardware(Spark ampMotor) {
-            this.ampMotor = ampMotor;
-        }
+    public Hardware(Spark ampMotor) {
+      this.ampMotor = ampMotor;
     }
-    // Initializes motors, amp object, and navx
-    private Spark m_ampMotor;
+  }
 
-    /**
-     * Create an instance of AmpSubsystem
-     * <p>
-     * NOTE: ONLY ONE INSTANCE SHOULD EXIST AT ANY TIME!
-     * <p>
-     * 
-     * @param ampHardware   Hardware devices required by amp
-     */
-    public AmpSubsystem(Hardware ampHardware) {
-        // Instantiates motors and navx
-        this.m_ampMotor = ampHardware.ampMotor;
+  private Spark m_ampMotor;
 
-        // Sets master motors inverted
-        m_ampMotor.setInverted(true);
-    }
+  /**
+   * Create an instance of AmpSubsystem
+   * <p>
+   * NOTE: ONLY ONE INSTANCE SHOULD EXIST AT ANY TIME!
+   * <p>
+   * 
+   * @param ampHardware  Hardware devices required by amp
+   */
+  public AmpSubsystem(Hardware ampHardware) {
+    this.m_ampMotor = ampHardware.ampMotor;
+  }
 
-    /**
-     * Initialize hardware devices for amp subsystem
-     * 
-     * @return hardware object containing all necessary devices for this subsystem
-     */
-    public static Hardware initializeHardware() {
-        Hardware ampHardware = new Hardware(
-            new Spark(Constants.AmpHardware.AMP_MOTOR_ID, MotorKind.NEO)
-        );
-        return ampHardware;
-    }
+  /**
+   * Initialize hardware devices for amp subsystem
+   * 
+   * @return Hardware object containing all necessary devices for this subsystem
+   */
+  public static Hardware initializeHardware() {
+    Hardware ampHardware = new Hardware(
+      new Spark(Constants.AmpHardware.AMP_MOTOR_ID, MotorKind.NEO)
+    );
+    return ampHardware;
+  }
 
-    /**
-     * Stop amp
-     */
-    public void stop() {
-        m_ampMotor.stopMotor();
-    }
 
-    //resets encoders
-    public void resetEncoders() {
-        m_ampMotor.resetEncoder();
-    }
+  /**
+   * Runs the amp motor to score
+   */
+  private void scoreAmp(double speed, double turn) {
+    m_ampMotor.set(speed, ControlType.kDutyCycle, turn, ArbFFUnits.kPercentOut);
+  }
 
-    // Controls the robot during teleop
-    private void teleop(double speed, double turn) {
-        m_ampMotor.set(speed, ControlType.kDutyCycle, -turn, ArbFFUnits.kPercentOut);
-    }
+  /**
+   * Stop amp
+   */
+  private void stop() {
+    m_ampMotor.stopMotor();
+  }
 
-    //do we need the turn request for the amp subsystem? does the motor need to turn?
-    public Command ampCommand(DoubleSupplier speedRequest, DoubleSupplier turnRequest) {
-        return run(() -> teleop(speedRequest.getAsDouble(), turnRequest.getAsDouble()));
-    }
+  /**
+   * Command to score a note in the amp
+   */
+  public Command scoreAmpCommand(DoubleSupplier speedRequest, DoubleSupplier turnRequest) {
+    return run(() -> scoreAmp(speedRequest.getAsDouble(), turnRequest.getAsDouble()));
+  }
 
-    // chan and nim and lyd are awesome
-    public Command stopCommand() {
-        return run(() -> stop());
-    }
-
-    @Override
-    public void periodic() {
-    // This method will be called once per scheduler run
-    }
-
-    @Override
-    public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation (for tests)
-    }
+  /**
+   * Stop Command
+   */
+  public Command stopCommand() {  // chan and nim and lyd are awesome
+    return run(() -> stop());
+  }
 }
