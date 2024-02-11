3184dd3431de7b7e158334d10a8c1f3a9d6b6bdc
rachitkakkar <rachitkakkar7@gmail.com>
2024-02-10 21:54:43 -0600

    Code cleanup (I changed one line lol)

--- src/test/java/frc/robot/subsystems/ShooterSubsystemTest.java
+++ src/test/java/frc/robot/subsystems/ShooterSubsystemTest.java
@@ -24,7 +24,6 @@ import com.revrobotics.CANSparkBase.ControlType;
 
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
